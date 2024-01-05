0ecea3a237f554e8af158c660c205740b29e90f9
Isackender <isack.info@gmail.com>
2024-01-05 12:22:01 +0100

    Typo

--- crossing/script.js
+++ crossing/script.js
@@ -115,7 +115,7 @@ $(document).ready(function() {
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
